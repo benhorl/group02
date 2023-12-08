c91e6e7b1e028fbd414110aa8ff1aa4ebc6841de
L3R0S <lrotte10@gmail.com>
2023-12-08 11:01:17 -0700

    Added more inserts to reviews

--- All Project code/init_data/insert.sql
+++ All Project code/init_data/insert.sql
@@ -1,3 +1,19 @@
 INSERT INTO users (username, password, preferences, state, city) values ('Trip', '$2a$12$Ew7Ka6cbeujJp1Ba6ioMSuN9uThNkcXsdYG53.FwIxnxMqPwlc34e', 'None', 'Colorado', 'Boulder');
 INSERT INTO posts (username, restaurant, located, content, rating, alias) values ('Trip', 'Sherpa Kitchen', 'Boulder', 'Excellent food', '5', 'sherpa-kitchen-boulder');
 INSERT INTO wishlist (username, restaurant, located, alias) values ('Trip', 'Sherpa Kitchen', 'Boulder', 'sherpa-kitchen-boulder');
+
+INSERT INTO users (username, password, preferences, state, city) values ('Levi', '$2y$12$YRBrEtk3twAbGiPzOb0jxO1goKrKUE9XYHbShmlXpLEZ7Cd8ryyh6', 'None', 'Colorado', 'Aurora');
+INSERT INTO posts (username, restaurant, located, content, rating, alias) values ('Levi', 'McDonalds', 'Boulder', 'Excellent food', '5', 'mcdonalds-boulder-5');
+INSERT INTO wishlist (username, restaurant, located, alias) values ('Levi', 'Sherpa Kitchen', 'Boulder', 'sherpa-kitchen-boulder');
+
+INSERT INTO users (username, password, preferences, state, city) values ('Ben', '$2y$12$qB09HPsA2sMQi2.JmYXmdeySjpOUXFd9pNLAHgSy8d2/a3.fZCtYC', 'None', 'Colorado', 'Boulder');
+INSERT INTO posts (username, restaurant, located, content, rating, alias) values ('Ben', 'T/ACOS', 'Boulder', 'Really enjoyed it', '5', 't-aco-an-urban-taqueria-boulder');
+INSERT INTO wishlist (username, restaurant, located, alias) values ('Ben', 'T/ACOS', 'Boulder', 't-aco-an-urban-taqueria-boulder');
+
+INSERT INTO users (username, password, preferences, state, city) values ('Jig', '$2y$12$pW.Gg2We3AZXPiLmvSwgKehKpmrK5nwQ4sCSF6aNWgIu1MwHC9bWK', 'None', 'Colorado', 'Boulder');
+INSERT INTO posts (username, restaurant, located, content, rating, alias) values ('Jig', 'Sherpa Kitchen', 'Boulder', 'Pretty Good', '4', 'sherpa-kitchen-boulder');
+INSERT INTO wishlist (username, restaurant, located, alias) values ('Jig', 'Sherpa Kitchen', 'Boulder', 'sherpa-kitchen-boulder');
+
+INSERT INTO users (username, password, preferences, state, city) values ('Joseph', '$2y$12$gOhd1zgRawnjnIyMQsnSGuV4HcwAiGlA.pDTKWSzIxSu9FPTYq.aC', 'None', 'Colorado', 'Boulder');
+INSERT INTO posts (username, restaurant, located, content, rating, alias) values ('Joseph', 'T/ACOS', 'Boulder', 'Its alright', '4', 't-aco-an-urban-taqueria-boulder');
+INSERT INTO wishlist (username, restaurant, located, alias) values ('Joseph', 'T/ACOS', 'Boulder', 't-aco-an-urban-taqueria-boulder');
